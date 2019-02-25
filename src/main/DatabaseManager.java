44b9f84efdd33564a99690562a10986955132bdd
Sebastian <sebastvi@stud.ntnu.no>
2019-02-25 13:28:05 +0100

    Added main-package, maven support and all that jazz. For real this time, brother.

---
+++ src/main/DatabaseManager.java
@@ -0,0 +1,11 @@
+package main;
+
+public class DatabaseManager {
+	private static final String DB_DRIVER_PATH = "com.mysql.cj.jdbc.Driver";
+	private static final String CONNECTION_STRING = "jdbc:mysql://mysql.stud.ntnu.no/ingebrin_dbproj?serverTimezone=UTC";
+	private static final String DB_USERNAME = "ingebrin_dbproj";
+	private static final String DB_PASSWORD = "dbproj";
+	
+	static int to = 2;
+	
+}
