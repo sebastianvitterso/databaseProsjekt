d60597dc550f2a0ddd7e3cf31c07a709c2587c2a
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2019-02-27 18:05:44 +0100

    Main methos main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         //TODO: Start hva som skal vises i console
+        String[] arguments = {"Apparat", "Øvelse"};
+        ConsoleManager.main(arguments);
     }
 }
