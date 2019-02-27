3dcf7eb9afd484599ad7ffa906514258a9c823df
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2019-02-27 20:00:48 +0100

    La til makeApparat og makeØvelseIØkt

--- src/main/ConsoleManager.java
+++ src/main/ConsoleManager.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class ConsoleManager {
 
+    public static String latestTimestamp = "";
+
     public static String getInput(String prompt) {
         System.out.println(prompt);
         String input = "";
@@ -26,7 +28,10 @@ public class ConsoleManager {
         String prestasjon = getInput("Prestasjon:");
         String notat = getInput("Notat(Optional):");
         boolean ok = QueryManager.addTreningsøkt(tidspunkt,varighet,form,prestasjon);
-        if (notat != ""){
+        if (ok){
+            latestTimestamp = tidspunkt;
+        }
+        if (notat != "" && ok){
             String id = "";
             List<Map<String,String>> treningsøkter = QueryManager.getTreningsøktID(tidspunkt);
             for (Map<String, String> row : treningsøkter){
@@ -43,6 +48,28 @@ public class ConsoleManager {
         printMapList(QueryManager.getApparat());
         String apparat_id = getInput("ApparatID:");
         return QueryManager.addØvelse(navn, beskrivelse, apparat_id);
+    }
+    public static boolean makeApparat(){
+        System.out.println("|-| Legg til apparat ved å fylle ut dette skjemaet |-|");
+        String navn = getInput("Navn:");
+        String beskrivelse = getInput("Beskrivelse:");
+        return QueryManager.addApparat(navn, beskrivelse);
+    }
+    public static boolean makeØvelseIØkt(){
+        System.out.println("|-| Legg til øvelse i økt ved å fylle ut dette skjemaet |-|");
+        printMapList(QueryManager.getØvelse());
+        String øvelse = getInput("Øvelse:");
+        String kilo = getInput("Kilo:");
+        String repetisjoner = getInput("Repetisjoner:");
+        String sett = getInput("Sett:");
+        String resultat = getInput("Resultat:");
+
+        String id = "";
+        List<Map<String,String>> treningsøkter = QueryManager.getTreningsøktID(latestTimestamp);
+        for (Map<String, String> row : treningsøkter){
+            id = row.get("treningsøkt_id");
+        }
+        return QueryManager.addØvelseIØkt(id, øvelse, kilo, repetisjoner, sett, resultat);
     }
 
     public static void printMapList(List<Map<String,String>> mapList) {
