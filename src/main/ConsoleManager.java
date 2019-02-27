97085316d8de16a46be19cc82cbf5fbbfd30648f
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2019-02-27 19:14:30 +0100

    Dette er en test som vi lgjøre deg eksplisitt lorem ipsum ConsoleManager data method for å laje til treningsøkti. Takk

--- src/main/ConsoleManager.java
+++ src/main/ConsoleManager.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ConsoleManager {
@@ -13,15 +15,35 @@ public class ConsoleManager {
         } catch(Exception e) {
             e.printStackTrace();
         }
-        System.out.println(String.format("Doing: %s", input));
         return input;
+    }
+
+    public static boolean makeTreningsøkt(){
+        System.out.println("|-| Legg til treningsøkt ved å fylle ut dette skjemaer |-|");
+        String tidspunkt = getInput("Tidspunkt(YYYY-MM-DD hh:mm:ss):");
+        String varighet = getInput("Varighet:");
+        String form = getInput("Form:");
+        String prestasjon = getInput("Prestasjon:");
+        String notat = getInput("Notat(Optional):");
+        boolean ok = QueryManager.addTreningsøkt(tidspunkt,varighet,form,prestasjon);
+        if (notat != ""){
+            String id = "";
+            List<Map<String,String>> treningsøkter = QueryManager.getTreningsøktID(tidspunkt);
+            for (Map<String, String> row : treningsøkter){
+                id = row.get("treningsøkt_id");
+            }
+            ok = QueryManager.addNotat(notat, id);
+        }
+        return ok;
     }
 
 
     public static void main(String[] args) {
-        for (String prompt : args){
+        /*for (String prompt : args){
             ConsoleManager.getInput(prompt);
-        }
+        }*/
+        System.out.println(makeTreningsøkt());
+
 
     }
 }
