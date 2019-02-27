46f8054d2a63d4ad1c69702507f3dce7fb34e615
Kobrestad <kaare@obrestad.net>
2019-02-27 19:26:22 +0100

    ny main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         //TODO: Start hva som skal vises i console
-        String[] arguments = {"Apparat", "Øvelse"};
-        ConsoleManager.main(arguments);
+        boolean finished = false;
+        while (!finished) {
+            String input = ConsoleManager.getInput("Hei, hva ønsker du å gjøre?").toLowerCase();
+            if(input.equals("slutt da") || input.equals("slutt")) {
+                finished = true;
+            } else if (input.equals("ny treningsøkt")){
+                ConsoleManager.makeTreningsøkt();
+            }
+        }
     }
 }
