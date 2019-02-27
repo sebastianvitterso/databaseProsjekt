3887a62414f090a786cd8b08c399ebb131a3b333
Asbjørn Toft <asbjotof@stud.ntnu.no>
2019-02-27 18:23:04 +0100

    bool

--- src/main/QueryManager.java
+++ src/main/QueryManager.java
@@ -8,25 +8,25 @@ import java.util.Map;
 
 public class QueryManager {
 
-    public static void addØvelse(String navn, String beskrivelse, String apparat_id){
+    public static boolean addØvelse(String navn, String beskrivelse, String apparat_id){
         String sql = String.format("INSERT INTO øvelse VALUES(%s,'%s','%s','%s')", "NULL", navn, beskrivelse, apparat_id);
-        DatabaseManager.sendUpdate(sql);
+        return((DatabaseManager.sendUpdate(sql)) > 0);
     }
-    public static void addTreningsøkt(String dato, String tidspunkt, String varighet, String form, String prestasjon, String notat_id){
+    public static boolean addTreningsøkt(String dato, String tidspunkt, String varighet, String form, String prestasjon, String notat_id){
         String sql = String.format("INSERT INTO treningsøkt VALUES(%s,'%s','%s','%s','%s','%s','%s')", "NULL", dato, tidspunkt, varighet, form, prestasjon, notat_id);
-        DatabaseManager.sendUpdate(sql);
+        return((DatabaseManager.sendUpdate(sql)) > 0);
     }
-    public static void addNotat(String tekst){
+    public static boolean addNotat(String tekst){
         String sql = String.format("INSERT INTO notat VALUES(%s,'%s')", "NULL", tekst);
         DatabaseManager.sendUpdate(sql);
     }
-    public static void addApparat(String navn, String beskrivelse){
+    public static boolean addApparat(String navn, String beskrivelse){
         String sql = String.format("INSERT INTO apparat VALUES(NULL,'%s', '%s')", navn, beskrivelse);
-        DatabaseManager.sendUpdate(sql);
+        return((DatabaseManager.sendUpdate(sql)) > 0);
     }
-    public static void addØvelseIØkt(String treningsøkt_id, String øvelse_id, String kilo, String repitisjoner, String sett, String resultat){
+    public static boolean addØvelseIØkt(String treningsøkt_id, String øvelse_id, String kilo, String repitisjoner, String sett, String resultat){
         String sql = String.format("INSERT INTO øvelse_i_økt VALUES('%s','%s','%s','%s','%s','%s')", treningsøkt_id, øvelse_id, kilo, repitisjoner, sett, resultat);
-        DatabaseManager.sendUpdate(sql);
+        return((DatabaseManager.sendUpdate(sql)) > 0);
     }
 
     public static List<Map<String, String>> getTreningsøkterMedNotat(){
@@ -40,6 +40,12 @@ public class QueryManager {
         String sql = String.format("SELECT resultat, kilo, repitisjoner, sett, tidspunkt FROM øvelse_i_økt NATURAL JOIN treningsøkt WHERE '/s' > tidspunkt ORDER BY tidspunkt", tidspunkt_input);
         return(DatabaseManager.sendQuery(sql));
     }
+
+    // Lage øvelsegrupper og finne øvelser som er i samme gruppe
+
+    
+
+
 
 
     public static void main(String[] args){
